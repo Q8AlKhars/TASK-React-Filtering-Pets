e38abeb3b752a3bfa33ea5f5d979c34c2dda69ce
Q8AlKhars <Mohammad.a.alkhars@gmail.com>
2024-11-13 20:03:13 +0300

    done

---
+++ src/Components/PetsList.js
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import pets from "../petsData";
+import PetItem from "./PetItem";
+
+function PetsList() {
+  const [searchText, setSearchText] = useState("");
+  const [type, setType] = useState("");
+
+  const filterByName = pets.filter((pet) =>
+    pet.name.toLowerCase().includes(searchTextgit)
+  );
+  const filterByType = filterByName.filter((pet) => pet.type.includes(type));
+
+  const petList = filterByType.map((pet) => <PetItem pet={pet} key={pet.id} />);
+
+  return (
+    <section id="doctors" className="doctor-section pt-140">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-xxl-5 col-xl-6 col-lg-7">
+            <div className="section-title text-center mb-30">
+              <h1 className="mb-25 wow fadeInUp" data-wow-delay=".2s">
+                Fur-ends
+              </h1>
+              <div className="input-group rounded">
+                <input
+                  type="search"
+                  className="form-control rounded"
+                  placeholder="Search"
+                  aria-label="Search"
+                  aria-describedby="search-addon"
+                  onChange={(event) => setSearchText(event.target.value)}
+                />
+              </div>
+              <br />
+              Type:
+              <select
+                onChange={(event) => setType(event.target.value)}
+                className="form-select"
+              >
+                <option value="" selected>
+                  All
+                </option>
+                <option value="Cat">Cat</option>
+                <option value="Dog">Dog</option>
+                <option value="Rabbit">Rabbit</option>
+              </select>
+            </div>
+          </div>
+        </div>
+
+        <div className="row justify-content-center">{petList}</div>
+      </div>
+    </section>
+  );
+}
+
+export default PetsList;
